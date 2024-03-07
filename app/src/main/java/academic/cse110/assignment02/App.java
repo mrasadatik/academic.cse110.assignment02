30fe480296d442bdf00447b83b5584b214a973bf
Md Asaduzzaman Atik <asadatik.dev@gmail.com>
2024-03-07 23:28:46 +0600

    Task 01 Completed

--- app/src/main/java/academic/cse110/assignment02/App.java
+++ app/src/main/java/academic/cse110/assignment02/App.java
@@ -1,11 +1,58 @@
 package academic.cse110.assignment02;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.Scanner;
+import academic.cse110.assignment02.tasks.*;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try (Scanner cliInput = new Scanner(System.in)) {
+            int taskChoice;
+            
+            System.out.println();
+            System.out.println("Assignment\t: 02");
+            System.out.println("\tSubmitted to\t: Ahmed Abdal Shafi Rasel (AASR), Lecturer, Department of CSE");
+            System.out.println();
+            System.out.println("Name\t\t: Md Asaduzzaman Atik");
+            System.out.println("Student ID\t: 2023-1-60-130");
+            System.out.println("Couse title\t: Object Oriented Programming");
+            System.out.println("Couse code\t: CSE110");
+            System.out.println("Section\t\t: 16");
+            System.out.println("Semester\t: Spring 2024");
+            
+            do {
+                System.out.println();
+                System.out.println();
+                System.out.println("Choose a task by entering the corresponding number:");
+                System.out.println("\t1. Palindrome Integer");
+                // System.out.println("\t2. Display Matrix of 0s and 1s");
+                // System.out.println("\t3. Check Password");
+                // System.out.println("\t4. Count the Letters in a String");
+                // System.out.println("\t5. Occurrences of a Specified Character");
+                // System.out.println("\t6. Stock Class");
+                // System.out.println("\t7. Use the GregorianCalendar Class");
+                // System.out.println("\t8. Stopwatch");
+                // System.out.println("\t9. Algebra: 2 * 2 Linear Equations");
+                // System.out.println("\t10. The Location Class");
+                
+                System.out.println();
+                System.out.println("\t\t0. Exit");
+                System.out.print("\nEnter your choice: ");
+                
+                taskChoice = cliInput.nextInt();
+                
+                System.out.println();
+                System.out.println();
+                
+                switch(taskChoice) {
+                    case 1 -> T01_Palindrome.runPalindrome(cliInput);
+                    case 0 -> {
+                        System.out.println("Exiting the program...");
+                        break;
+                    }
+                    default -> System.out.println("Invalid choice. Please try again.");
+                }
+                
+            } while (taskChoice != 0);
+        }
     }
 }
---
+++ app/src/main/java/academic/cse110/assignment02/tasks/T01_Palindrome.java
@@ -0,0 +1,34 @@
+package academic.cse110.assignment02.tasks;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mrasadatik
+ */
+public class T01_Palindrome {
+    private static int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
+
+    private static boolean isPalindrome(int number) {
+        return number == reverse(number);
+    }
+
+    public static void runPalindrome(Scanner scanner) {
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+    }
+}
